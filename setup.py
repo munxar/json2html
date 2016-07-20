63e4dbb3d79a9087bde9176f2b5fe17be1c42545
munxar <sascha.aeppli@liip.ch>
2016-07-20 06:49:25 +0200

    bump version to 1.0.2
    added python 3.5 classifiers

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info[:2] < (2,7):
 setup(
     name = 'json2html',
     packages = ['json2html'],
-    version = '1.0.1',
+    version = '1.0.2',
     install_requires=required,
     description = 'JSON to HTML Table Representation',
     long_description=open('README.rst').read(),
@@ -25,5 +25,6 @@ setup(
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
     ),
 )
